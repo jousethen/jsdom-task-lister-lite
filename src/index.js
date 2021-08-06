4f3c858f8905144c2b141403524ea76aaca579ae
jousethen <jthen93@gmail.com>
2021-08-06 13:42:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.getElementById("create-task-form").addEventListener('submit', function (event) {
+    let list = document.getElementById("tasks");
+    let task = document.createElement("li");
+
+    task.innerHTML = document.getElementById("new-task-description").value;
+    list.appendChild(task);
+    event.preventDefault();
+  });
+
 });
